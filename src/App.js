35d2cf51cffd31f80b5d59e5d253c81b6aab96b7
caleb.xu.77@gmail.com <caleb.xu.77@gmail.com>
2022-02-27 09:36:48 -0600

    added keys and fixed ids

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@
  * (users will also have a chance to edit time limits within the window of time they process their tasks)
  * If the user does not process their tasks, all will be swiped left by default
  * Tasks that are continually swiped left will gradually appear more unpleasant (as incentive to remove them)
+ * 
+ * Next Steps:
+ * Change the data structure to a single array
+ * Separate Weekly and Daily Stats into separate components
  */
 
 import {useState} from 'react';
 import "./App.css";
 
-function TodolistRow({todoName}) {
+function TodolistRow({todo}) {
   return (
     <tr>
       <td>
@@ -20,7 +24,7 @@ function TodolistRow({todoName}) {
             <input type="checkbox">
             </input>
             <label>
-              {todoName}
+              {todo.name}
             </label>
           </form>
         </button>
@@ -59,14 +63,13 @@ function TodolistTable({todos, stats, onTodosChange, onStatsChange}) {
     if(category !== lastCategory) {
       rows.push(
         <TodolistCategoryRow 
-          category={category}
-          key={}/>
+          category={category} />
       );
       lastCategory = category;
     }
     rows.push(
       <TodolistRow
-        todoName = {todo.name} />
+        todo={todo} />
     );
   });
 
@@ -105,7 +108,7 @@ function UtilitiesBar({stats, todos, setTodos, setStats}) {
           [{
             name: input, 
             swipes: 0, 
-            id: this.name,
+            id: this.name.toLowerCase().split(" ").join("_"),
           }].concat(todos)
         );
       } else {
@@ -116,10 +119,10 @@ function UtilitiesBar({stats, todos, setTodos, setStats}) {
 
   return (
     <div className='ubuntu-font'>
-      <button onClick={handleDayClick}>Daily Summary</button>
+      <button className="utilities-button" onClick={handleDayClick}>Daily Summary</button>
       {showDaily && <p>Tasks Completed: {stats.completedToday}</p>}
 
-      <button onClick={handleWeekClick}>Weekly Summary</button>
+      <button className="utilities-button" onClick={handleWeekClick}>Weekly Summary</button>
       {showWeekly && <p>Tasks Completed: {stats.completedThisWeek}</p>}
 
       <form>
@@ -128,7 +131,7 @@ function UtilitiesBar({stats, todos, setTodos, setStats}) {
           placeholder="New Todo..." 
           onKeyDown={handleKeyDown}></input>
       </form>
-      <button >Check Up</button>
+      <button className="utilities-button check-up-button">Check Up</button>
       <h3>Left/Right Ratio: {stats.swipedLeft} : {stats.swipedRight}</h3>
     </div>
   );
@@ -137,13 +140,13 @@ function UtilitiesBar({stats, todos, setTodos, setStats}) {
 function TodolistPage() {
   const [todos, setTodos] = useState(
     [
-      {name: "todo 1", swipes: 0, id: this.name},
-      {name: "todo 6", swipes: 1, id: this.name}, 
-      {name: "todo 7", swipes: 1, id: this.name},
-      {name: "todo 3", swipes: 1, id: this.name},
-      {name: "todo 4", swipes: 3, id: this.name},
-      {name: "todo 2", swipes: 4, id: this.name},
-      {name: "todo 5", swipes: 5, id: this.name},
+      {name: "todo 1", swipes: 0, id: "todo 1"},
+      {name: "todo 6", swipes: 1, id: "todo 6"}, 
+      {name: "todo 7", swipes: 1, id: "todo 7"},
+      {name: "todo 3", swipes: 1, id: "todo 3"},
+      {name: "todo 4", swipes: 3, id: "todo 4"},
+      {name: "todo 2", swipes: 4, id: "todo 2"},
+      {name: "todo 5", swipes: 5, id: "todo 5"},
     ]
   );
   const [stats, setStats] = useState(
